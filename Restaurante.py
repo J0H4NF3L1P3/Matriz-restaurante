7d1b94c3d1eefad6da8fa6361d63ff3ec7d49f91
J0H4NF3L1P3 <roncancioguzmanjohanfelipe@gmail.com>
2026-05-20 13:11:24 -0500

    Solucion Problema 2

--- Restaurante.py
+++ Restaurante.py
@@ -1,10 +1,10 @@
 # =====================================================================
 # Curso: Fundamentos de Programación
 # Fase 5 - Evaluación Final POA
-# Solución al Problema 2: Gestión de Precios de Menú de Restaurante
+# Solución Problema 2: Gestión de Precios de Menú de Restaurante
+# Nombre del estudiante: Johan Felipe Roncancio Guzman
 # =====================================================================
 
-# MÓDULO / FUNCIÓN: Calcular el precio final aplicando la lógica de negocio
 def calcular_precio_final(categoria_producto, precio_base, categoria_objetivo, umbral):
     """
     Determina el precio final de un producto aplicando un 15% de descuento
@@ -16,13 +16,13 @@ def calcular_precio_final(categoria_producto, precio_base, categoria_objetivo, u
     else:
         return precio_base
 
-# BLOQUE PRINCIPAL / PROCESAMIENTO ESTRUCTURADO
+
 def main():
     print("====================================================")
     print("      SISTEMA DE GESTIÓN DE PRECIOS - RESTAURANTE   ")
     print("====================================================")
     
-    # 1. Definición de la Matriz (Estructura de datos estática)
+    
     menu_restaurante = [
         ["Hamburguesa Gourmet", "Comida Rápida", 25000],
         ["Papas Nativas", "Comida Rápida", 8000],
@@ -32,7 +32,7 @@ def main():
         ["Brownie con Helado", "Postres", 10500]
     ]
     
-    # 2. Parámetros fijos de la promoción
+ 
     categoria_promocion = "Bebidas"
     umbral_precio = 8000
     
@@ -42,13 +42,13 @@ def main():
     print(f"{'PRODUCTO':<23} | {'CATEGORÍA':<15} | {'PRECIO BASE':<13} | {'PRECIO FINAL':<13}")
     print("-" * 73)
     
-    # 3. Recorrido de la matriz mediante estructura repetitiva (Ciclo for)
+   
     for producto in menu_restaurante:
         nombre = producto[0]
         categoria = producto[1]
         precio_base = producto[2]
         
-        # Llamado al módulo pasándole los parámetros requeridos
+      
         precio_final = calcular_precio_final(categoria, precio_base, categoria_promocion, umbral_precio)
         
         print(f"{nombre:<23} | {categoria:<15} | ${precio_base:<12,} | ${precio_final:<12,.2f}")
